d6ebc663196f96a79109a2983d71899c6b6a5d9e
Muhammad Saad <muhammadsaad28102003@gmail.com>
2024-05-03 23:46:15 +0500

    Time: 357 ms (70.78%) | Memory: 48.6 MB (93.24%) - LeetSync

---
+++ 79-word-search/word-search.js
@@ -0,0 +1,34 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    const m = board.length;
+    const n = board[0].length;
+    
+    const dfs = (i, j, k) => {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== word[k]) return false;
+        if (k === word.length - 1) return true;
+        
+        const temp = board[i][j];
+        board[i][j] = '*'; // Marking as visited
+        
+        const found = dfs(i + 1, j, k + 1) ||
+                      dfs(i - 1, j, k + 1) ||
+                      dfs(i, j + 1, k + 1) ||
+                      dfs(i, j - 1, k + 1);
+        
+        board[i][j] = temp; // Backtrack
+        
+        return found;
+    };
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (dfs(i, j, 0)) return true;
+        }
+    }
+    
+    return false;
+};
